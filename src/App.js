83aa2c875714eff8dfbd63631bc67aadf381ecac
uyghur-io <admin@uyghur.io>
2020-07-07 15:01:17 +0300

    -

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Uyghur Fonts
         </a>
       </header>
     </div>
