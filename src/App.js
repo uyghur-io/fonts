7a51ab6fb073a07555f848119c3a83205914a2b8
uyghur-io <admin@uyghur.io>
2020-07-08 13:36:25 +0300

    -

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Test from './components/Test.js'
+import TestTwo from './components/TestTwo.js'
 
 function App() {
   return (
     <div className="App">
-      
+      <Test />
+      <TestTwo />
     </div>
   );
 }
---
+++ src/components/MainNav.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MainNav() {
+    return(
+        <div></div>
+    )
+}
+
+export default MainNav
